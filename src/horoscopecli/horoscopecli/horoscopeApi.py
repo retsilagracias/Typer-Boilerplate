57e982b2b808a0da6bcaafa77ba747ae1ba11b85
Retsila <alister.gracias@epita.fr>
2020-12-30 19:47:43 +0100

    [API] Added horoscopeApi to project

---
+++ src/horoscopecli/horoscopecli/horoscopeApi.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+def getHoroscopeApi(sign: str, flag: str):
+    try:
+        r = requests.get(f"http://horoscope-api.herokuapp.com/horoscope/{flag}/{sign}")
+        if r.status_code != 200:
+            return (False, "ERROR: Sorry, we have found an issue with the API we are currently using.")
+        rText = json.loads(r.text)
+        return (True, rText["horoscope"])
+    except:
+        return (False, "ERROR: Sorry, your request didn't went through, bad network error.")    
