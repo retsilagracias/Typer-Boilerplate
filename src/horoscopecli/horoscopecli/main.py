e3751eb3ceb365df00217692b14574503057d700
Thomas Franel <thomas.franel77@gmail.com>
2020-12-30 20:02:39 +0100

    [TRANSLATION] - initialized the translation part of the project, using decarator

--- src/horoscopecli/horoscopecli/main.py
+++ src/horoscopecli/horoscopecli/main.py
@@ -1,6 +1,26 @@
 import typer
+from googletrans import Translator
 
 app = typer.Typer()
+
+def translateDeco(function):
+    def deco(args, **kwargs):
+        typer.echo("\nBefore translation / Avant traduction:\n")
+        texts = args[0]
+        for text in texts:
+            typer.echo(text)
+        typer.echo("\n====================\n")
+        translations = function(args, **kwargs)
+        typer.echo("After translation / Après traduction:\n")
+        for translation in translations:
+            typer.echo(translation.text)
+    return deco
+
+@translateDeco
+def gTranslate(text: list[str], src: str, dest: str):
+    translator = Translator()
+    translations = translator.translate(text, dest=dest, src=src)
+    return translations
 
 if __name__ == "__main__":
     app()
