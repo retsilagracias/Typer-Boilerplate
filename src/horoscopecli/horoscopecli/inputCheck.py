4c1cf80506308823dbf2a0bd94cbdad5e56a40e6
picou75 <jeanne.gardebois@gmail.com>
2020-12-30 19:53:18 +0100

    add validInputCategoryOption

--- src/horoscopecli/horoscopecli/inputCheck.py
+++ src/horoscopecli/horoscopecli/inputCheck.py
@@ -25,3 +25,14 @@ def validInputDateOption(today: bool, week: bool, month: bool, year: bool):
     if len(flags) == 0:
         return (True, "today", "daily")
     return (True, flags[0][0], flags[0][1])
+
+
+def validInputCategoryOption(love: bool, work: bool, finance: bool, self: bool, family: bool):
+    args = [love, work, finance, self, family]
+    labels = ["love", "work", "finance", "self", "family"]
+    flags = [labels[i] for i in range(5) if args[i]]
+    if len(flags) > 1:
+        return (False, "ERROR: It seems you have put more than one option, chose between LOVE, WORK, FINANCE, SELF or FAMILY.")
+    if len(flags) == 0:
+        return (True, "self")
+    return (True, flags[0])
