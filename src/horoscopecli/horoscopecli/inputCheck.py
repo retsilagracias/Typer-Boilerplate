8da13598c78647c4c9b463d62301ba128f240a6f
picou75 <jeanne.gardebois@gmail.com>
2020-12-30 19:53:00 +0100

    add validInputDate function

--- src/horoscopecli/horoscopecli/inputCheck.py
+++ src/horoscopecli/horoscopecli/inputCheck.py
@@ -15,3 +15,13 @@ def validInputSign(sign: str):
     elif sign.lower() in signsFr:
         return (True, signsEn[signsFr.index(sign.lower())], sign.lower())
     return (False, "ERROR: It seems you have entered a wrong astrological sign.", "")
+
+def validInputDateOption(today: bool, week: bool, month: bool, year: bool):
+    args = [today, week, month, year]
+    labels = [("today", "daily"), ("week", "weekly"), ("month", "monthly"), ("year","yearly")]
+    flags = [labels[i] for i in range(4) if args[i]]
+    if len(flags) > 1:
+        return (False, "ERROR: It seems you have put more than one option, chose between DAY, WEEK, MONTH or YEAR.")
+    if len(flags) == 0:
+        return (True, "today", "daily")
+    return (True, flags[0][0], flags[0][1])
